178a70cc0e71de2dfaea401ebb26535ce092e6e1
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-07-01 19:12:03 -0500

    Website footer complete

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import HeroImg from '../components/HeroImg'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <NavBar/>
-      <HeroImg></HeroImg>
+      <HeroImg/>
+      <Footer/>
     </div>
   )
 }
