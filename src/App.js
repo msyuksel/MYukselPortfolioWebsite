41aed3d761ebef597b8ac7123dfe1b7bb1dca7a9
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-07-03 18:46:50 -0500

    Fixed bug where banner Projects button wasn't working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
     <Routes>
       <Route path= "/" element = {<Home/>}/>
-      <Route path= "/project" element = {<Project/>}/>
+      <Route path= "/projects" element = {<Project/>}/>
       <Route path= "/about" element = {<About/>}/>
       <Route path= "/contact" element = {<Contact/>}/>
 
