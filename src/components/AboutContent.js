009b4d91ccc7d7b86925ed197f493b1658741f26
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-07-03 17:38:32 -0500

    Finished projects page and able to add projects now

---
+++ src/components/AboutContent.js
@@ -0,0 +1,11 @@
+import "./AboutContentStyles.css";
+
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div>AboutContent</div>
+  )
+}
+
+export default AboutContent
--- src/routes/About.js
+++ src/routes/About.js
@@ -3,15 +3,17 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 
 
 const About = () => {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <HeroImg2 heading="ABOUT ME"
         text="I'm a developer" />
-      <Footer/>
+      <AboutContent />
+      <Footer />
     </div>
   )
 }
