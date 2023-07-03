8619297f44609ddfbac20955c11fae6dab5b20fd
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-07-02 19:53:43 -0500

    Formatting and text changes

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -6,30 +6,30 @@ import IntroImg from "../assets/cup-of-coffee-1280537.jpg";
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
-  return (
-    <div className = "hero">
-        <div className="mask">
-            <img className="intro-img"
-                src={IntroImg} alt ="IntroImg"/>
-        </div>
-        <div className="content">
-            <p>
-                HI, I'M A DEVELOPER.
-            </p>
-            <h1>
-               Developer 
-            </h1>
-            <div>
-                <Link to= "/project" className="btn">
-                    Projects
-                </Link>
-                <Link to= "/Contact" className="btn btn-light">
-                    Contact
-                </Link>
+    return (
+        <div className="hero">
+            <div className="mask">
+                <img className="intro-img"
+                    src={IntroImg} alt="IntroImg" />
+            </div>
+            <div className="content">
+                <p>
+                    HI, I'M A DEVELOPER.
+                </p>
+                <h1>
+                    SDET, MOBILE/WEB, AND UNITY 3D.
+                </h1>
+                <div>
+                    <Link to="/project" className="btn">
+                        Projects
+                    </Link>
+                    <Link to="/Contact" className="btn btn-light">
+                        Contact
+                    </Link>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default HeroImg
